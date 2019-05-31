f5daf141375f09cc5236e00548c2c599e2b62308
Daegun Choi <43776784+ChoiysApple@users.noreply.github.com>
2019-05-31 20:29:28 +0900

    bottom bar fragment

--- App/Sonagi/app/src/main/java/com/example/price_this/sonagi/SelcetionFragment.java
+++ App/Sonagi/app/src/main/java/com/example/price_this/sonagi/SelcetionFragment.java
@@ -5,12 +5,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
-public class SelectionFragment extends Fragment {
+public class SelcetionFragment extends Fragment {
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_selection, container, false);
+
 
         return rootView;
     }
