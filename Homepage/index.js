e2b957593f3a772fcf6ec37beae49a16cba0cc2f
Luc!Das <jimin61445@naver.com>
2019-05-31 22:04:29 +0900

    js파일 옮김

---
+++ Homepage/index.js
@@ -0,0 +1,12 @@
+function head_nav(n) 
+{
+    if(n=='f'){
+        document.getElementById("main").src="find.html";
+    }
+    if(n=='r'){
+        document.getElementById("main").src="register.html";
+    }
+    if(n=='c'){
+        document.getElementById("main").src="credit.html";
+    }
+}
